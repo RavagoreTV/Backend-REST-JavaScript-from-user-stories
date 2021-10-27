6f27346594d537b2905e03815c776f6955f08300
Victor Dahl Saltoft <vict2729@edu.easj.dk>
2021-10-27 12:56:54 +0200

    Mapper og classer tilføjet

---
+++ Backend REST + JavaScript from user stories/Models/MusicRecords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_REST___JavaScript_from_user_stories.Model
+{
+    public class MusicRecords
+    {
+        public string Titel { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+
+    }
+}
