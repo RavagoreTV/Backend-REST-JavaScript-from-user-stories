5f98b7b6a2c7f583ae022512cd5efd6bc9b36558
Peter Albinussen <pete47m0@edu.easj.dk>
2021-11-03 12:40:02 +0100

    nu med statuskoder

--- Backend REST + JavaScript from user stories/Controllers/MusicsController.cs
+++ Backend REST + JavaScript from user stories/Controllers/MusicsController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Backend_REST___JavaScript_from_user_stories.Managers;
 using Backend_REST___JavaScript_from_user_stories.Model;
+using Microsoft.AspNetCore.Http;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,38 +17,72 @@ namespace Backend_REST___JavaScript_from_user_stories.Controllers
     {
         private readonly MusicRecordManagers _musicRecordManagers = new MusicRecordManagers();
         // GET: api/<MusicsController>
+
         [HttpGet]
-        public IEnumerable<MusicRecords> GetAll([FromQuery] string title, [FromQuery] string artist, [FromQuery] string sortBy)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<IEnumerable<MusicRecords>> GetAll([FromQuery] string title, [FromQuery] string artist,
+            [FromQuery] string sortBy)
         {
-            return _musicRecordManagers.GetAll(title, artist, sortBy);
+            var musicRecordsList = _musicRecordManagers.GetAll(title, artist, sortBy);
+            if (musicRecordsList.Count == 0)
+            {
+                return NoContent();
+            }
+
+            return Ok(musicRecordsList);
         }
 
         // GET api/<MusicsController>/5
         [HttpGet("{title}")]
-        public MusicRecords GetByTilte(string title)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<MusicRecords> GetByTilte(string title)
         {
-            return _musicRecordManagers.GetByTitle(title);
+            var musicRecords = _musicRecordManagers.GetByTitle(title);
+            if (musicRecords == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(musicRecords);
         }
 
         //POST api/<MusicsController>
         [HttpPost]
-        public MusicRecords Post([FromBody] MusicRecords value)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult<MusicRecords> Post([FromBody] MusicRecords value)
         {
-            return _musicRecordManagers.Add(value);
+            return Ok(_musicRecordManagers.Add(value));
         }
 
         // PUT api/<MusicsController>/5
         [HttpPut("{title}")]
-        public MusicRecords Put(string title, [FromBody] MusicRecords value)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult <MusicRecords> Put(string title, [FromBody] MusicRecords value)
         {
-            return _musicRecordManagers.Update(title, value);
+            var musicRecords = _musicRecordManagers.Update(title, value);
+            if (musicRecords == null)
+            {
+                return NotFound();
+            }
+            return Ok( musicRecords);
         }
 
         // DELETE api/<MusicsController>/5
         [HttpDelete("{title}")]
-        public MusicRecords Delete(string title)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult <MusicRecords> Delete(string title)
         {
-            return _musicRecordManagers.Delete(title);
+            var musicRecords = _musicRecordManagers.Delete(title);
+            if (musicRecords == null)
+            {
+                return NotFound();
+            }
+            return Ok( musicRecords);
         }        
     }
 }
